42d06967378e5c79ecad3397c1d6972174ef65d8
tajinder-dhoot <singhtazinder047@gmail.com>
2022-11-20 23:15:13 -0500

    implemented featured products on home page

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -31,7 +31,7 @@ const products_reducer = (state, action) => {
     };
   }
   if (action.type === "GET_PRODUCTS_ERROR") {
-    return { ...state, products_loading: true, products_error: false };
+    return { ...state, products_loading: false, products_error: true };
   }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
